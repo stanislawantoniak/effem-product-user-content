dda874bd402da44bfe9689cef6e664806c8a99eb
Stanislaw Antoniak <stanislaw.h.antoniak@gmail.com>
2020-04-02 11:19:16 +0200

    logging

--- src/index.js
+++ src/index.js
@@ -10,28 +10,28 @@ dotenv.config();
 const { buildFederatedSchema } = require('@apollo/federation');
 
 const schema = buildFederatedSchema([{
-		typeDefs,
-		resolvers
-	}]);
+	typeDefs,
+	resolvers
+}]);
 
 const api = new UserContentAPI();
 
 const server = new ApolloServer({
 	schema: schema,
-  dataSources: () => ({
-    userContentAPI: api
-  }),
-  engine: {
+	dataSources: () => ({
+		userContentAPI: api
+	}),
+    engine: {
 		apiKey: process.env.AGM_API_KEY,
 		schemaTag: process.env.AGM_SCHEMA_TAG
-  }
+	}
 });
 
-//api.getProductById('xx').then( 
-//		result => console.log('sample review: ', result) ,
-//		error => console.error( error)
-//		);
+api.getProductById('xx').then( 
+		result => console.log('sample review: ', result) ,
+		error => console.error( error)
+		);
 
 server.listen({ port: process.env.PORT || 4003 }).then(({ url }) => {
-  console.log(`🚀 Server ready at ${url}`);
+	console.log(`🚀 Server ready at ${url}`);
 });
--- src/resolvers.js
+++ src/resolvers.js
@@ -1,6 +1,7 @@
 module.exports = {
 	Product: {
 		__resolveReference(product, { dataSources }) {
+			console.log('resolver starting');
 			return dataSources.userContentAPI.getProductById(product.id)
 		}
 	}
